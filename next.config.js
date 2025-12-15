a427a6e23ec8f3de2316bfc36d4d7deddea0819c
Jason Zeng <jasonzeng124@gmail.com>
2025-12-15 14:29:50 -0500

    i think ci works

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ module.exports = withMDX({
     pageExtensions: ['js', 'mdx'],
     basePath: basePath,
     assetPrefix: basePath,
+    // static export mode
+    output: 'export',
     trailingSlash: true,
     env: {
         NEXT_PUBLIC_BASE_PATH: basePath,
